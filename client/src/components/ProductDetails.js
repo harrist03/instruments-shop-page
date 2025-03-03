ee7cdc092b9851dbce9e7311cff60c26d86fbcbb
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-03-03 12:15:36 +0100

    ProductDetails image display done but code structure needs to be improved TODO - ProductDetails code structure

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -10,27 +10,62 @@ export default class ProductDetails extends Component {
 
         this.state = {
             product: {},
+            mainImage: "",
             redirectToDisplayAllProducts: localStorage.accessLevel < ACCESS_LEVEL_GUEST
         }
     }
 
-    componentDidMount() {
+    handleImageClick = (img) => {
+        this.setState({ mainImage: img })
 
+        axios.get(`${SERVER_HOST}/products/image/${img.filename}`)
+            .then(res => {
+                if (res.data.image) {
+                    document.getElementById("mainImage").src = `data:image/png;base64,${res.data.image}`
+                }
+            })
+    }
+
+    componentDidMount() {
         axios.get(`${SERVER_HOST}/products/${this.props.match.params.id}`, { headers: { "authorization": localStorage.token } })
             .then(res => {
                 if (res.data) {
                     if (res.data.errorMessage) {
                         console.log(res.data.errorMessage)
                     } else {
-                        this.setState({ product: res.data })
-                        console.log("Product details retrieved")
+                        this.setState({
+                            product: res.data,
+                            mainImage: res.data.images.length > 0 ? res.data.images[0] : ""
+                        }, () => {
+                            if (this.state.product.images && this.state.product.images.length > 0) {
+                                axios.get(`${SERVER_HOST}/products/image/${this.state.product.images[0].filename}`)
+                                    .then(res => {
+                                        if (res.data.image) {
+                                            document.getElementById("mainImage").src = `data:image/png;base64,${res.data.image}`
+                                        }
+                                    })
+                                    // .catch(err => console.error(`Error loading image ${image.filename}`, err))
+                                const imagePromises = this.state.product.images.map(image =>
+                                    axios.get(`${SERVER_HOST}/products/image/${image.filename}`)
+                                        .then(res => {
+                                            if (res.data.image) {
+                                                document.getElementById(image._id).src = `data:image/png;base64,${res.data.image}`
+                                            }
+                                        })
+                                        .catch(err => console.error(`Error loading image ${image.filename}`, err))
+                                )
+
+                                Promise.all(imagePromises).then(() => {
+                                    console.log("All images loaded successfully")
+                                })
+                            }
+                        })
                     }
-                } else {
-                    console.log("Product not found")
                 }
             })
             .catch(err => console.error("Error fetching product:", err))
     }
+
 
     render() {
         const { product } = this.state
@@ -40,13 +75,28 @@ export default class ProductDetails extends Component {
                 <div className="product-details-container">
                     <div className="product-details">
                         <div className="product-image">
-                            {/*{product.images.forEach(image => */}
-                            {/*    <img src={image.file} key={image.id} />*/}
-                            {/*    alt={product.name}*/}
-                            {/*    />*/}
-                            {/*)}*/}
+                            {this.state.mainImage ? (
+                                <img
+                                    id="mainImage"
+                                    alt={product.name}
+                                    className="main-image"/>
+                                ) : (
+                                <p>No images available</p>
+                            )}
 
+                            <div className="thumbnail-container">
+                                {product.images && product.images.map(image=> (
+                                    <img
+                                        key={image._id}
+                                        id={image._id}
+                                        alt={product.name}
+                                        className="thumbnail-image"
+                                        onClick={() => this.handleImageClick(image)}
+                                    />
+                                ))}
+                            </div>
                         </div>
+
                         <div className="product-info">
                             <h2>{product.name}</h2>
                             <p className="details">{product.category}</p>
